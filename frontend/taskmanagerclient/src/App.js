f22c98651626b768afcfe9e3a8e0806da2cedd21
Marvafathima <marvafathima62@gmail.com>
2024-10-10 20:14:56 +0530

    hot response handled successfully

--- frontend/taskmanagerclient/src/App.js
+++ frontend/taskmanagerclient/src/App.js
@@ -1,12 +1,15 @@
 
-import './App.css';
+
 import AuthForm from './components/AuthForm';
 import TaskManager from './components/TaskManager';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProtectedRoute from './routes/ProtectedRoute';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <Router>
+      <ToastContainer />
     <div>
     <Routes>
     <Route path="" element={<AuthForm/>} />
